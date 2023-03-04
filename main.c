3bd6cb5c4d38b8ba78c4d88b3f34b2170a8611b1
Bruno Rodrigues Faria <80405594+brunofaria27@users.noreply.github.com>
2023-03-04 17:21:56 -0300

    main, com alocação de memoria - NÃO TEM MUITA

--- main.c
+++ main.c
@@ -4,13 +4,18 @@
 #include <time.h>
 #define PI 3.141592
 
-/************************************************************************/
-void DetSinCos(unsigned char element, float *sin_element, float *cos_element) {
-  static float sin_table[256], cos_table[256];
+void DetSinCos(unsigned char element, float *sin_element, float *cos_element)
+{
+  static float *sin_table = NULL, *cos_table = NULL;
   static int initialized = 0;
 
-  if (!initialized) {
-    for (int i = 0; i < 256; i++) {
+  if (!initialized)
+  {
+    sin_table = (float *)calloc(256, sizeof(float));
+    cos_table = (float *)calloc(256, sizeof(float));
+
+    for (int i = 0; i < 256; i++)
+    {
       sin_table[i] = sin(2.0 * PI * i / 360.0);
       cos_table[i] = cos(2.0 * PI * i / 360.0);
     }
@@ -21,35 +26,36 @@ void DetSinCos(unsigned char element, float *sin_element, float *cos_element) {
   *cos_element = cos_table[element];
 }
 
-/************************************************************************/
-
-void DetOutputTable(float *C, float *out_even_table, float *out_odd_table) {
+void DetOutputTable(float *C, float *out_even_table, float *out_odd_table)
+{
   float sin_element, cos_element;
 
-  for (int i = 0; i < 256; i++) {
+  for (int i = 0; i < 256; i++)
+  {
     DetSinCos(i, &sin_element, &cos_element);
     out_even_table[i] = 2.0 * sin_element + exp(cos_element);
     out_odd_table[i] = exp(sin_element) * C[i];
   }
 }
 
-/************************************************************************/
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   FILE *data_file;
   int rows, cols, i, j;
   double time_spent = 0.0;
   unsigned char element, *M;
-  float out_even, out_odd, *Q, C[256], out_even_table[256], out_odd_table[256], pow_even_table[256], odd_table[256];
+  float out_even, out_odd, *Q, *C = NULL, *out_even_table = NULL, *out_odd_table = NULL, *pow_even_table = NULL, *odd_table = NULL;
 
   clock_t begin = clock();
 
-  if (argc < 4) {
+  if (argc < 4)
+  {
     printf("Sintaxe: %s file_name rows cols\n", argv[0]);
     exit(-1);
   }
-  
-  if (NULL == (data_file = fopen(argv[1], "rb"))) {
+
+  if (NULL == (data_file = fopen(argv[1], "rb")))
+  {
     printf("File not found.\n");
     exit(-1);
   }
@@ -57,24 +63,32 @@ int main(int argc, char **argv) {
   rows = atoi(argv[2]);
   cols = atoi(argv[3]);
 
-  if (!(M = (unsigned char *)malloc((long)rows * cols))) {
-    printf("Not enough memory to read file.\n");
+  M = (unsigned char *)malloc(rows * cols * sizeof(unsigned char));
+  if (M == NULL)
+  {
+    printf("Error allocating memory for M.\n");
     exit(-1);
   }
 
-  if (cols * rows != fread(M, 1, cols * rows, data_file)) {
+  if (cols * rows != fread(M, 1, cols * rows, data_file))
+  {
     printf("Error reading data file.\n");
     exit(-1);
   }
   fclose(data_file);
 
-  if (!(Q = (float *)malloc((long)rows * cols * sizeof(float)))) {
-    printf("Not enough memory for output matrix.\n");
+  C = (float *)calloc(256, sizeof(float));
+  if (C == NULL)
+  {
+    printf("Error allocating memory for C.\n");
     exit(-1);
   }
 
-  for (i = 0; i < 256; i++)
-    C[i] = 0.0;
+  if (!(Q = (float *)malloc((long)rows * cols * sizeof(float))))
+  {
+    printf("Not enough memory for output matrix.\n");
+    exit(-1);
+  }
 
   for (i = 0; i < cols * rows; i++)
     C[M[i]]++;
@@ -82,15 +96,45 @@ int main(int argc, char **argv) {
   for (i = 0; i < 256; i++)
     C[i] = (C[i] > 0) ? log(C[i]) : 0.0;
 
+  out_even_table = (float *)calloc(256, sizeof(float));
+  if (out_even_table == NULL)
+  {
+    printf("Error allocating memory for out_even_table.\n");
+    exit(-1);
+  }
+
+  out_odd_table = (float *)calloc(256, sizeof(float));
+  if (out_odd_table == NULL)
+  {
+    printf("Error allocating memory for out_odd_table.\n");
+    exit(-1);
+  }
+
+  pow_even_table = (float *)calloc(256, sizeof(float));
+  if (pow_even_table == NULL)
+  {
+    printf("Error allocating memory for pow_even_table.\n");
+    exit(-1);
+  }
+
+  odd_table = (float *)calloc(256, sizeof(float));
+  if (odd_table == NULL)
+  {
+    printf("Error allocating memory for odd_table.\n");
+    exit(-1);
+  }
+
   DetOutputTable(C, out_even_table, out_odd_table);
-  for (i = 0; i < 256; i++) {
+  for (i = 0; i < 256; i++)
+  {
     if ((i % 2) == 0)
       pow_even_table[i] = pow(out_even_table[i], 2);
     else
       odd_table[i] = out_odd_table[i];
   }
 
-  for (i = 0; i < cols * rows; i++) {
+  for (i = 0; i < cols * rows; i++)
+  {
     element = M[i];
     if ((element % 2) == 0)
       Q[i] = pow_even_table[element];
@@ -101,23 +145,30 @@ int main(int argc, char **argv) {
   clock_t end = clock();
 
   /**** Saída não deve ser medida ****/
-  /*
   data_file = fopen("out", "w");
 
-  if (data_file == NULL) {
+  if (data_file == NULL)
+  {
     printf("Error to create file.\n");
     exit(-1);
   }
 
-  for (i = 0; i < rows; i++) {
-    for (j = 0; j < cols; j++) {
+  for (i = 0; i < rows; i++)
+  {
+    for (j = 0; j < cols; j++)
+    {
       fprintf(data_file, "%f ", *(Q + j * cols + i));
     }
     fprintf(data_file, "\n");
   }
 
   fclose(data_file);
-  */
+  free(M);
+  free(C);
+  free(out_even_table);
+  free(out_odd_table);
+  free(pow_even_table);
+  free(odd_table);
   free(Q);
 
   time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
